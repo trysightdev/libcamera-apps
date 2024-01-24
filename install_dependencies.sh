7ccbd563e27500e84ceed41f4708cb694b34f888
trysightdev <109621244+trysightdev@users.noreply.github.com>
2024-01-24 10:24:59 -0500

    Create install_dependencies.sh
    Installs dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Exit if any command fails
+set -e
+
+# Update package list
+sudo apt update
+
+# Install FreeType
+echo "Installing FreeType..."
+wget http://download.savannah.gnu.org/releases/freetype/freetype-2.13.2.tar.gz
+tar -xvzf freetype-2.13.2.tar.gz
+cd freetype-2.13.2
+mkdir build && cd build
+cmake ..      # generates Makefile + deactivates HarfBuzz if not found
+make          # compile libs
+sudo make install  # install libs & headers
+cd ../..
+
+# Install other dependencies
+echo "Installing other dependencies..."
+sudo apt install -y pigpio pigpiod
+sudo apt install -y libcamera-dev libepoxy-dev libjpeg-dev libtiff5-dev libegl1-mesa-dev libpng-dev
+sudo apt install -y cmake libboost-program-options-dev libdrm-dev libexif-dev
+sudo apt install -y xserver-xorg xinit x11-xserver-utils
+
+echo "Installation complete."
